3bfcd0bceb4140c0b76740749cf2259503a0ca04
SHAYAN ABRAR <shayanabrar7@gmail.com>
2024-10-18 05:01:59 +0600

    Initial commit

---
+++ simple arithmetic calculator.py
@@ -0,0 +1,18 @@
+operator = input("Enter an Operator (+, -,*, /): ")
+num1 = float(input("Enter the First number: "))
+num2 = float(input("Enter the Second number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is not a valid operator")
